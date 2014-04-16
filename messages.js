7769e15e00dd99adfddb2cda1473c46e7801a7f2
Stephie Wu <saxyclarinette@gmail.com>
2014-04-16 18:21:38 -0400

    Messages now have an expanding reply box
    Added an expanding textarea jQuery library.
    No send button yet.

--- messages.js
+++ messages.js
@@ -5,20 +5,35 @@
 $(function() {
     // when you click on a message table, it shows the contents of the message.
     $( "table.message-table" ).click(function() {
-        var content = $(this).find( "div.message-content" );
+        var content = $(this).find( "div.message-container" );
         if (!content.is(":visible"))
         {
-            content.slideToggle( "fast" );
             event.stopPropagation();
+            content.slideToggle( "fast", function () {
+                var textarea = content.find("textarea.expanding");
+                if (!textarea.expanding('active')) {
+                    textarea.expanding();
+                }
+            });
         }
     });
 
     $( "tr.message-header" ).click(function() {
-        var content = $(this).closest('table').find( "div.message-content" );
+        var content = $(this).closest('table').find( "div.message-container" );
         if (content.is(":visible"))
         {
             content.slideToggle( "fast" );
             event.stopPropagation();
         }
     });
+
+
+    $( "button" ).button({
+        icons: {
+            primary: "ui-icon-arrowreturnthick-1-w"
+        }
+    });
+
+    $()
+
 });
