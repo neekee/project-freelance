b26183b2893ae25e7ad841cc6bcdf6a58cd4e5d2
niki <niki@mit.edu>
2014-04-16 23:55:55 -0400

    aaaand here goes

--- project.js
+++ project.js
@@ -1,12 +1,17 @@
 $(function() {
 
-	var upload = function() {
-		$("#choose-file").click();
-	};
 
-	$("#upload-button").click(upload);
+	$("#upload-button").click(function() {
+		var file = $("#choose-file").prop('files');
+		if (file.length == 0) {
+			$("#uploaded").html("Select a file first.");
+		} else {
+			$("#uploaded").html("Uploaded new draft!");
+		}
+	});
 
 	$("#save-button").click(function() {
+		event.preventDefault();
 		console.log("called save");
 		$("#saved").html("Saved!");
 	});
