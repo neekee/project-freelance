fd20c22b89e38361ed671fa1c022a14cda18a813
Stephie Wu <saxyclarinette@gmail.com>
2014-04-16 21:45:58 -0400

    Mark messages as read upon opening

--- messages.js
+++ messages.js
@@ -2,46 +2,53 @@
  * Created by Stephie on 4/16/14.
  */
 
-$(function() {
+$(function () {
     // when you click on a message table, it shows the contents of the message.
-    $( "table.message-table" ).click(function() {
-        var content = $(this).find( "div.message-container" );
-        if (!content.is(":visible"))
-        {
-            event.stopPropagation();
-            content.slideToggle( "fast", function () {
+    $("table.message-table").click(function () {
+        var content = $(this).find("div.message-container");
+        if (!content.is(":visible")) {
+            content.slideToggle("fast", function () {
                 var textarea = content.find("textarea.expanding");
                 if (!textarea.expanding('active')) {
                     textarea.expanding();
                 }
             });
+            // mark read if necessary
+            if ($(this).hasClass('unread')) {
+                var icon = $(this).find("span.ui-icon-mail-closed");
+                icon.removeClass("ui-icon-mail-closed");
+                icon.addClass("ui-icon-mail-open");
+                $(this).removeClass('unread');
+            }
+            event.stopPropagation();
         }
     });
 
-    $( "tr.message-header" ).click(function() {
-        var content = $(this).closest('table').find( "div.message-container" );
-        if (content.is(":visible"))
-        {
-            content.slideToggle( "fast" );
+    $("tr.message-header").click(function () {
+        var content = $(this).closest('table').find("div.message-container");
+        if (content.is(":visible")) {
+            content.slideToggle("fast");
             event.stopPropagation();
         }
     });
 
 
-    $( "button.message-send-reply" ).button({
+    $("button.message-send-reply").button({
         icons: {
             primary: "ui-icon ui-icon-mail-closed"
         }
     });
 
-    $( "button.message-choose-file" ).button({
+    $("button.message-choose-file").button({
         icons: {
-            primary: "ui-icon ui-icon-mail-closed"
+            primary: "ui-icon ui-icon-folder-open"
         }
     });
 
-    $("#select_logo").click(function(){
-        $("#logo").trigger('click');
+    $(".message-choose-file").click(function () {
+        var input = $(this).closest('div.message-reply').find("input.input-file");
+        input.trigger('click');
+        event.stopPropagation();
         return false;
     });
 
