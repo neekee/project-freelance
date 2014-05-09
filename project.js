8722d36bb239039bf6e4b69c2f1f0f00666b3660
niki <niki@mit.edu>
2014-05-09 04:09:54 -0400

    can add project. is not persistent

--- project.js
+++ project.js
@@ -15,4 +15,19 @@ $(function() {
 		console.log("called save");
 		$("#saved").html("Saved!");
 	});
+
 });
+
+function addProject(textboxID) {
+	console.log("got here")
+	var textBox = document.getElementById(textboxID);
+	var text = textBox.value;
+	if (text == "") {
+		return;
+	}
+	var lastProject = $(".project").last()
+	console.log(lastProject);
+	var string = "<p><a class=\"project\" href=\"" + text.replace(/\s+/g, '').toLowerCase() + ".html\">" + text + "</a></p>";
+	lastProject.after(string);
+	textBox.value = "";
+}
