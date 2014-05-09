1631ea7ffeeba190d8ae1f7f6cb9361ae9be7f9b
Michelle Szucs <mszucs@mit.edu>
2014-05-09 06:14:00 -0400

    Can add new task folders...lots of new task folders...

--- tasks.js
+++ tasks.js
@@ -35,6 +35,76 @@ $(function() {
 
 });
 
+function addFolder() {
+    var folderName = document.getElementById('newfoldername').value;
+    document.getElementById('newfoldername').value = "";
+    var table = $("<table>").addClass("task-table")
+        .append($("<tr>").addClass("task-header")
+            .append($("<td>").addClass("message-icon-read")
+                .append($("<span>").addClass("ui-icon").addClass("ui-icon ui-icon-folder-collapsed")))
+            .append($("<td>").addClass("task-label")
+                .text(folderName))
+            .append($("<td>").addClass("message-icon-toggle").attr("rowspan", 3)
+                    .append($("<span>").addClass("ui-icon ui-icon-carat-1-s"))))
+        .append($("<tr>")
+            .append($("<td>"))
+            .append($("<td>").attr("colspan", 2))
+                .append($("<div>").addClass("message-container")
+                        .append($("<div>").addClass("message-content"))
+                        .append($("<input>").attr("type","text").addClass("tasks-add-field"))
+                        .append($("<button>").addClass("tasks-add-button").text("Add task"))
+                )
+        )
+    $(".task-table").after(table);
+    addExpandListener(table);
+    buttonifyChildren(table);
+}
+
+function addExpandListener(someElement) {
+    someElement.click(function (evt) {
+        var content = $(this).find( "div.task-container" );
+        var icon = $(this).find("td.message-icon-toggle > span.ui-icon");
+        icon.addClass("ui-icon-carat-1-n");
+        icon.removeClass("ui-icon-carat-1-s");
+        content.slideToggle( "fast" );
+        event.stopPropagation();
+});
+}
+
+function buttonifyChildren (someParent) {
+        someParent.find("input[type=submit], input[type=button], button")
+            .click(function( event ) {
+                event.preventDefault();
+            })
+            .bind('mouseup', function() {
+                $(this).blur()});
+    }
+
+/**
+<table class="task-table">
+        <tr class="task-header">
+          <td class="message-icon-read"><span class="ui-icon ui-icon-folder-collapsed"></span></td>
+          <td class="task-label">General
+          <td class="message-icon-toggle" rowspan="3"><span class="ui-icon ui-icon-carat-1-s"></span></td>
+        </tr>
+        <tr>
+          <td colspan="2">
+            <div class="task-container">
+              <div class="task-content">
+                <input type="text" id="generaltext" name="New task" class="tasks-add-field">
+                <input type="button" id="AddGeneral" name="Add" value="Add" class="tasks-add-button"onclick ="addTask('General','generaltext')">
+                <br>
+                <table id="General" class="task-table">
+                </table>
+              </div>
+            </div>
+          </td>
+        </tr>
+      </table>
+    </div>
+
+    **/
+
  // following code adapted from http://viralpatel.net/blogs/dynamically-add-remove-rows-in-html-table-using-javascript/
 
 function addTask(tableID,textboxID) {
@@ -51,7 +121,7 @@ function addTask(tableID,textboxID) {
     newcell.innerHTML = string;
     textBox.value = "";
 }
-    function deleteRow(tableID) {
+function deleteRow(tableID) {
             try {
             var table = document.getElementById(tableID);
             var rowCount = table.rows.length;
